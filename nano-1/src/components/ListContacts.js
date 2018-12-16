6fcfca5b1daa293bd528c997f0bf5ce618879cdd
rocky122 <38912058+rocky122@users.noreply.github.com>
2018-12-16 23:54:15 +0530

    filtering the contacts and showing the number of contactgs

--- nano-1/src/components/ListContacts.js
+++ nano-1/src/components/ListContacts.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 import PropTypes from "prop-types";
 
-class ListContacts extends React.Component {
+class ListContacts extends Component {
   // To add propTypes to a class we can use this method
   // The older method defining propTypes is generally used
   // for functional components receiving the props
@@ -17,10 +17,19 @@ class ListContacts extends React.Component {
     this.setState(() => ({
       query: query.trim()
     }));
-    
   };
+  clearQuery = () => this.updateQuery("");
   render() {
+    const { query } = this.state;
     const { contacts, removeContact } = this.props;
+
+    const showingContacts =
+      query === ""
+        ? contacts
+        : contacts.filter(c =>
+            c.name.toLowerCase().includes(query.toLowerCase())
+          );
+
     return (
       <div className="list-contacts">
         {/* {JSON.stringify(this.state.query)} */}
@@ -29,12 +38,22 @@ class ListContacts extends React.Component {
             type="text"
             className="search-contacts"
             placeholder="Search Contact"
-            value={this.state.query}
+            value={query}
             onChange={e => this.updateQuery(e.target.value)}
           />
         </div>
+
+        {showingContacts.length !== contacts.length ? (
+          <div className="showing-contacts">
+            <span>
+              Now showing {showingContacts.length} contacts of {contacts.length}
+            </span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        ) : null}
+
         <ol className="contact-list">
-          {contacts.map(contact => (
+          {showingContacts.map(contact => (
             <Contact
               key={contact.id}
               contact={contact}
